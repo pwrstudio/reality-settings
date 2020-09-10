cbefc1d19c8c3b402033a1df3c0fbcc66b54dc94
Hanna Nilsson <hannanilsson@Hannas-MBP.fritz.box>
2020-09-10 12:02:58 +0200

    Work on settings view

--- src/Components/Settings.svelte
+++ src/Components/Settings.svelte
@@ -14,15 +14,16 @@
   let seed = random(0, 65535)
   let seedArray = []
 
-  $: {
-    let seedToBits = (seed >>> 0).toString(2)
+  let seedToBits = (seed >>> 0).toString(2)
 
-    while (seedToBits.length < 16) {
-      seedToBits = seedToBits + '0'
-    }
-
-    seedArray = seedToBits
+  while (seedToBits.length < 16) {
+    seedToBits = seedToBits + '0'
   }
+
+  seedArray = seedToBits.split('')
+  // console.log(seedArray)
+  // console.log(Array.isArray(seedArray))
+
 
   const padToTwo = (number) =>
     number <= 9999 ? `00000${number}`.slice(-5) : number
@@ -31,23 +32,29 @@
 <style lang="scss">
   @import '../variables.scss';
 
-  .settings {
-    padding: 30px;
-    font-size: 32px;
-    max-width: 1000px;
-    margin-left: auto;
-    margin-right: auto;
+  $CELL_DIMENSION: 120px;
+  $CELL_DIMENSION_PHONE: 80px;
 
-    @include screen-size('small') {
-      font-size: 16px;
-    }
+
+  .settings {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+  }
+
+  .settings-inner {
+    // background: red;
+
 
     .header {
       font-family: 'five', 'Akkurat-Mono', monospace;
-      font-size: 96px;
+      font-size: 42px;
       margin-bottom: 30px;
       text-align: center;
-      -webkit-text-stroke-width: 5px;
+      -webkit-text-stroke-width: 2px;
       -webkit-text-stroke-color: #222222;
       letter-spacing: -2px;
 
@@ -63,14 +70,7 @@
     .parameter {
       margin-bottom: 20px;
       display: flex;
-      justify-content: space-between;
-
-      .label {
-        display: inline-block;
-        @include screen-size('small') {
-          display: none;
-        }
-      }
+      justify-content: center;
 
       .preview {
         display: inline-block;
@@ -80,23 +80,16 @@
       }
     }
 
-    input {
-      width: 100%;
-      display: block;
-      margin-right: 20px;
-      margin-left: 20px;
-    }
-
     .run {
+      height: $CELL_DIMENSION * 0.75;
+      line-height: $CELL_DIMENSION * 0.75;
+      width: 4 *  $CELL_DIMENSION;
       cursor: pointer;
       outline: 0;
       border: 0;
-      background: white;
+      background: lightgray;
       text-decoration: none;
       color: #222222;
-      padding: 10px 40px;
-      margin-top: 30px;
-      width: 100%;
       display: block;
       text-align: center;
       transition: background 0.5s $transtion2;
@@ -105,37 +98,53 @@
       margin-right: auto;
 
       &:hover {
-        background: lightgray;
+        background: white;
       }
+
+      @include screen-size('small') {
+      height:  $CELL_DIMENSION_PHONE;
+    width:  $CELL_DIMENSION_PHONE * 4;
+    line-height:  $CELL_DIMENSION_PHONE;
+
+    }
+
     }
   }
 
   .sigil {
-    width: 300px;
-    height: 300px;
+    width: 4 *  $CELL_DIMENSION;
+    height:4 *  $CELL_DIMENSION;
 
     background: grey;
     margin-left: auto;
     margin-right: auto;
-    margin-bottom: 60px;
+    margin-bottom: $CELL_DIMENSION_PHONE * 0.25;
     @include screen-size('small') {
-      width: 200px;
-      height: 200px;
-      margin-bottom: 30px;
+      width: 4 *  $CELL_DIMENSION_PHONE;
+    height:4 *  $CELL_DIMENSION_PHONE;
+
     }
   }
 
   .cell {
-    height: 75px;
-    width: 75px;
-    border-radius: 75px;
-    line-height: 75px;
+    height:  $CELL_DIMENSION;
+    width:  $CELL_DIMENSION;
+    border-radius:  $CELL_DIMENSION;
+    line-height:  $CELL_DIMENSION;
+    cursor: pointer;
+    color: black;
+    // font-size: 16px;
+    font-size: 52px;
+
+
 
     @include screen-size('small') {
-      height: 50px;
-      width: 50px;
-      border-radius: 50px;
-      line-height: 50px;
+      height:  $CELL_DIMENSION_PHONE;
+    width:  $CELL_DIMENSION_PHONE;
+    border-radius:  $CELL_DIMENSION_PHONE;
+    line-height:  $CELL_DIMENSION_PHONE;
+    font-size: 32px;
+
     }
 
     // display: inline-block;
@@ -143,200 +152,48 @@
     background: #c4c4c4;
     background: grey;
     background: rgb(57, 227, 57);
-    font-size: 2px;
     text-align: center;
     color: #333333;
-    // display: inline-block;
-    // transform: rotateZ(45deg);
-    // background: green;
-    // background: #00ff00;
-
-    // &:nth-child(even) {
-    //   background: #848484;
-    //   background: #c4c4c4;
-    //   // background: #00ff00;
-    // }
-    transition: none;
 
     .text {
       display: none;
     }
 
+    .plus, .minus  {
+      display: none;
+      color: white;
+        opacity: 0.5;
+        user-select: none;
+    }
+
+    transition: background 0.3s ease-out;
+
+    &:hover {
+      .plus {
+        display: block;
+
+      }
+    }
+
     &.alive {
       border-radius: 25px;
-
-      // transition: background 0.3s ease-out;
       background: orangered;
-      // transform: rotateZ(90deg);
-    }
-  }
+      transition: background 0.3s ease-out;
 
-  // Styling Cross-Browser Compatible Range Inputs with Sass
-  // Github: https://github.com/darlanrod/input-range-sass
-  // Author: Darlan Rod https://github.com/darlanrod
-  // Version 1.5.2
-  // MIT License
 
-  $track-color: #333333 !default;
-  $thumb-color: orangered !default;
+      &:hover {
+        .minus {
+          display: block;
+        }
 
-  $thumb-radius: 40px !default;
-  $thumb-height: 40px !default;
-  $thumb-width: 40px !default;
-  $thumb-shadow-size: 0px !default;
-  $thumb-shadow-blur: 0px !default;
-  $thumb-shadow-color: rgba(0, 0, 0, 0) !default;
-  $thumb-border-width: 0px !default;
-  $thumb-border-color: #eceff1 !default;
-
-  $track-width: 100% !default;
-  $track-height: 6px !default;
-  $track-shadow-size: 0px !default;
-  $track-shadow-blur: 0px !default;
-  $track-shadow-color: rgba(0, 0, 0, 0) !default;
-  $track-border-width: 0px !default;
-  $track-border-color: #cfd8dc !default;
-
-  $track-radius: 0px !default;
-  $contrast: 5% !default;
-
-  $ie-bottom-track-color: darken($track-color, $contrast) !default;
-
-  @mixin shadow($shadow-size, $shadow-blur, $shadow-color) {
-    box-shadow: $shadow-size $shadow-size $shadow-blur $shadow-color,
-      0 0 $shadow-size lighten($shadow-color, 5%);
-  }
-
-  @mixin track {
-    cursor: default;
-    height: $track-height;
-    transition: all 0.2s ease;
-    width: $track-width;
-  }
-
-  @mixin thumb {
-    // @include shadow(
-    //   $thumb-shadow-size,
-    //   $thumb-shadow-blur,
-    //   $thumb-shadow-color
-    // );
-    background: $thumb-color;
-    border: $thumb-border-width solid $thumb-border-color;
-    border-radius: $thumb-radius;
-    box-sizing: border-box;
-    cursor: default;
-    height: $thumb-height;
-    width: $thumb-width;
-  }
-
-  [type='range'] {
-    -webkit-appearance: none;
-    background: transparent;
-    margin: $thumb-height / 2 0;
-    width: $track-width;
-
-    &::-moz-focus-outer {
-      border: 0;
-    }
-
-    &:focus {
-      outline: 0;
-
-      &::-webkit-slider-runnable-track {
-        background: lighten($track-color, $contrast);
-      }
-
-      &::-ms-fill-lower {
-        background: $track-color;
-      }
-
-      &::-ms-fill-upper {
-        background: lighten($track-color, $contrast);
+        .plus {
+          display: none;
+        }
+    
       }
     }
 
-    &::-webkit-slider-runnable-track {
-      @include track;
-      @include shadow(
-        $track-shadow-size,
-        $track-shadow-blur,
-        $track-shadow-color
-      );
-      background: $track-color;
-      border: $track-border-width solid $track-border-color;
-      border-radius: $track-radius;
-    }
 
-    &::-webkit-slider-thumb {
-      @include thumb;
-      -webkit-appearance: none;
-      margin-top: (
-        (-$track-border-width * 2 + $track-height) / 2 - $thumb-height / 2
-      );
-    }
-
-    &::-moz-range-track {
-      @include shadow(
-        $track-shadow-size,
-        $track-shadow-blur,
-        $track-shadow-color
-      );
-      @include track;
-      background: $track-color;
-      border: $track-border-width solid $track-border-color;
-      border-radius: $track-radius;
-      height: $track-height / 2;
-    }
-
-    &::-moz-range-thumb {
-      @include thumb;
-    }
-
-    &::-ms-track {
-      @include track;
-      background: transparent;
-      border-color: transparent;
-      border-width: ($thumb-height / 2) 0;
-      color: transparent;
-    }
-
-    &::-ms-fill-lower {
-      @include shadow(
-        $track-shadow-size,
-        $track-shadow-blur,
-        $track-shadow-color
-      );
-      background: $ie-bottom-track-color;
-      border: $track-border-width solid $track-border-color;
-      border-radius: ($track-radius * 2);
-    }
-
-    &::-ms-fill-upper {
-      @include shadow(
-        $track-shadow-size,
-        $track-shadow-blur,
-        $track-shadow-color
-      );
-      background: $track-color;
-      border: $track-border-width solid $track-border-color;
-      border-radius: ($track-radius * 2);
-    }
-
-    &::-ms-thumb {
-      @include thumb;
-      margin-top: $track-height / 4;
-    }
-
-    &:disabled {
-      &::-webkit-slider-thumb,
-      &::-moz-range-thumb,
-      &::-ms-thumb,
-      &::-webkit-slider-runnable-track,
-      &::-ms-fill-lower,
-      &::-ms-fill-upper {
-        cursor: not-allowed;
-      }
-    }
   }
 
   .read-only {
@@ -350,67 +207,34 @@
 </style>
 
 <div class="settings" use:links>
-  <div class="header">Reality Settings</div>
 
-  <!-- <div class="parameter">
-    <div class="label">Salt</div>
-    <input min="0" max="10" step="0.01" type="range" bind:value={salt} />
-    <div class="preview">{salt}</div>
-  </div>
-  <div class="parameter">
-    <div class="label">Heat</div>
-    <input min="0" max="100" type="range" bind:value={heat} />
-    <div class="preview">{heat}</div>
-  </div>
-  <div class="parameter">
-    <div class="label">Repulsion</div>
-    <input min="0" max="100" type="range" bind:value={size} />
-    <div class="preview">{size}</div>
-  </div>
-  <div class="parameter">
-    <div class="label">Friction</div>
-    <input min="0" max="100" type="range" bind:value={friction} />
-    <div class="preview">{friction}</div>
-  </div> -->
+  <div class="settings-inner">
+      <!-- <div class="header">Reality Settings</div> -->
 
-  <!-- <div class="parameter top">
-    <div class="label">Affinity</div>
-    <input min="0" max="100" type="range" bind:value={parameterA} />
-    <div class="preview">{padToTwo(parameterA)}</div>
-  </div>
+    <!-- <div class="parameter bottom">
+      <div class="preview">Seed: {seedArray.join('')}</div>
+    </div> -->
 
-  <div class="parameter right">
-    <div class="label">Gravity</div>
-    <input min="0" max="100" type="range" bind:value={parameterB} />
-    <div class="preview">{padToTwo(parameterB)}</div>
-  </div>
+    <div class="sigil">
+      {#each seedArray as cell, index}
+        <div class="cell" class:alive={cell == '1'} on:click={e =>{
+            seedArray[index] = seedArray[index] == '1' ? '0' : '1';
+            console.dir(seedArray)
+          }}>
+          <div class='plus'>+</div>
+          <div class='minus'>-</div>
+          </div>
+      {/each}
+    </div>
 
-  <div class="parameter left">
-    <div class="label">Heat</div>
-    <input min="0" max="100" type="range" bind:value={parameterC} />
-    <div class="preview">{padToTwo(parameterC)}</div>
-  </div> -->
-
-  <div class="sigil">
-    {#each seedArray as cell}
-      <div class="cell" class:alive={cell == '1'} />
-    {/each}
-  </div>
-
-  <div class="parameter bottom">
-    <div class="label small">Seed</div>
-    <input min="0" max="65535" type="range" bind:value={seed} />
-    <div class="preview">{padToTwo(seed)}</div>
-    <!-- <div class="preview"></div> -->
-  </div>
-
-  <div class="parameter">
-    <div
-      class="run"
-      on:click={() => {
-        navigate('/seed/' + (seed >>> 0).toString(2))
-      }}>
-      Start
+    <div class="parameter">
+      <div
+        class="run"
+        on:click={() => {
+          navigate('/seed/' + seedArray.join(''))
+        }}>
+        Seed: {seedArray.join('')}
+      </div>
     </div>
   </div>
 </div>
